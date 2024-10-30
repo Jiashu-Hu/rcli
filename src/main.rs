1b7bb080757b939984f91b360f20798eb78302ec
Teddy <teddy@Teddys-MacBook-Pro-2.local>
2024-10-30 12:46:06 -0500

    1.1 refactor opts related code to opts.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use std::{fs, path::Path};
+use std::fs;
+use clap::Parser;
+use rcli::{Opts, SubCommand};
 // we are using Serde, it can Cast the input data to internal data structure
 // and then cast to output.  
 // Serilize is a trait that can be derived to make a serde data structure serializable to JSON, XML, etc.
 // Deserialize is a trait that can be derived to make a struct deserializable to serde data structure.
 use serde::{Serialize, Deserialize};
 // rcli csv -i input.csv -o output.json --header -d ','
-use clap::Parser;
 use csv::Reader;
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -23,40 +24,6 @@ struct Player {
     nationality: String,
     #[serde(rename = "Kit Number")]
     kit: u8,
-}
-
-#[derive(Debug, Parser)]
-
-// name = "rcli" is the name of the command,
-// it will get version and author from the Cargo.toml
-#[command(name = "rcli", version, author, about, long_about = None)]
-struct Opts {
-    #[command(subcommand)]
-    cmd: SubCommand,
-}
-
-/// Subcommands of the rcli command, typed to [`Opts::cmd`]
-#[derive(Debug, Parser)]
-enum SubCommand {
-    #[command(name = "csv", about = "show csv or convert csv to other formats")]
-    // CsvOpts is the struct that will be used to parse the arguments
-    Csv(CsvOpts),
-}
-
-/// Options for the csv subcommand, parsed to [`SubCommand::Csv(CsvOpts)`](SubCommand::Csv)
-#[derive(Debug, Parser)]
-struct CsvOpts {
-    #[arg(short, long, value_parser = verify_input_file)]
-    input: String,
-    
-    #[arg(short, long, default_value = "output.json")]
-    output: String,
-    
-    #[arg(short, long, default_value_t = ',')]
-    delimiter: char,
-    
-    #[arg(long, default_value_t = true)]
-    header: bool,
 }
 
 fn main() -> anyhow::Result<()> {
@@ -91,20 +58,4 @@ fn main() -> anyhow::Result<()> {
     }
 
     Ok(())
-}
-
-/// verify_input_file is a value parser for the [`CsvOpts::input`] argument, it will check if the file exists.  
-/// Please note that this function is only check for file name, not the content of the file, it will
-/// return ok if the file exists, otherwise it will return an error.
-
-// static: if there any thing has the same lifetime with process, it can be static
-fn verify_input_file(filename: &str) -> Result<String, &'static str> {
-    // path::new is a function from the std::path module
-    // it will create a Path object from the filename
-    // exists() is a method of the Path object, it will return true if the file exists
-    if Path::new(filename).exists() {
-        Ok(filename.into())
-    } else {
-        Err("File does not exist")
-    }
 }
